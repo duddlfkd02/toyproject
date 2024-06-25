a8c64916f82b4c1a90541ff8e3dd4658c200eda8
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-06-25 17:54:28 +0900

    switch문 활용 및 기타 코드 수정

--- calculator/app.js
+++ calculator/app.js
@@ -1,31 +1,41 @@
 const showNum = document.querySelector('.showNumber');
-const result = document.querySelector('.result');
-const operator = document.querySelectorAll("button.operator");
+
+let operNum = 0; // 입력한 값을 담을 변수
+let selectNum1 = 0;
+
 
 function input(num) {
     showNum.value += num;
 }
 
-let calcNum = [];
-
-operator.forEach((button) => {
-    button.addEventListener('click', () => {
-        calcNum.push(showNum.value);
-        calcNum.push(button.innerHTML);
-        console.log(calcNum);
-        showNum.value = '';
-    })
-})
-
-result.addEventListener('click', () => {
-    calcNum.push(showNum.value);
-    showNum.value = eval(calcNum.join(''));
-})
-
+function operator(oper1) {
+    selctOper = oper1 // 연산자 저장
+    selectNum1 = parseFloat(showNum.value); // 숫자 정수로 바꾸기
+    showNum.value = ''; // input 창은 비운다
+}
+// 계산 함수
+function calculate() {
+    let selectNum2 = parseFloat(showNum.value);
+    switch (selctOper) {
+        case '+':
+            result = selectNum1 + selectNum2;
+            break;
+        case '-':
+            result = selectNum1 - selectNum2;
+            break;
+        case '*':
+            result = selectNum1 * selectNum2;
+            break;
+        case '/':
+            result = selectNum1 / selectNum2;
+            break;
+    }
+    showNum.value = result;
+}
 
 //리셋버튼
 function reset() {
     showNum.value = '';
-    calcNum.length = 0;
+
 }
 
