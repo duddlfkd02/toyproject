f525d35111c52c4d810281d75cae72426925461d
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-06-26 18:06:39 +0900

    날씨 수정, api 불러오기 중간..

---
+++ WeatherApp/app.js
@@ -0,0 +1,13 @@
+// window.addEventListener('load', () => {
+//     let long;
+//     let lat;
+
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(position => {
+//             console.log(position); // 현재 내 위치 알려줌
+//             long = position.coords.longitude;
+//             lat = position.coords.latitude;
+//         })
+//     }
+// });
+
