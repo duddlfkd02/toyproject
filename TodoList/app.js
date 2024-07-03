8d22f0c1d0ab95a477e3ebb8912f54683e1950aa
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-07-03 13:09:04 +0900

    로컬 불러오기 전

--- TodoList/app.js
+++ TodoList/app.js
@@ -1,29 +1,16 @@
 const addContent = document.querySelector('.input_box');
 const listInner = document.querySelector('.list_inner');
-let todoList = document.querySelectorAll('.list');
-// const resetBtn = document.querySelector('.resetBtn');
-let memos = document.querySelectorAll('.input-box');
 
 
-// //메모 리스트 스토리지에 저장
-// function saveMemo() {
-//     localStorage.setItem('memos', listInner.innerHTML);
-//     console.log(memos);
-// }
+//할일 추가
+function plusContent(storageData) { // 매개변수
+    let todoContents = addContent.value;
+    if (storageData) {
+        todoContents = storageData.contents //contains로 가져온거 문자로 바꿈
+        console.log(todoContents)
+    }
+    if (addContent.value !== '') { //값이 빈칸이 아닐 때 추가
 
-
-
-// //저장된 리스트 불러오기
-// function showMemo() {
-//     listInner.innerHTML = localStorage.getItem('memos');
-// }
-// showMemo();
-
-
-
-//추가버튼 누르면 리스트 추가
-function plusContent() {
-    if (addContent.value !== '') {
         const listInner = document.querySelector('.list_inner');
         let list = document.createElement('li');
         let deleteBtn = document.createElement('img');
@@ -46,6 +33,10 @@ function plusContent() {
         newBtn.addEventListener('click', () => {
             list.classList.toggle('complete');
         })
+        //완료된 것도 로컬에 저장
+        if (storageData && storageData.complete === true) {
+            button.classList.add('complete');
+        }
         saveMemo();
     }
 
@@ -55,20 +46,21 @@ function plusContent() {
 function deleteList(e) {
     if (e.target.tagName === 'IMG') {
         e.target.parentElement.remove();
-        // saveMemo();
     }
+    saveMemo();
 }
 
-
+//전체 초기화
 function reset() {
     const listAll = document.querySelectorAll('.list_inner li');
     for (let i = 0; i < listAll.length; i++) {
         listAll[i].remove();
     }
+    saveMemo();
 }
 
+//로컬에 목록 저장
 function saveMemo() {
-
     const saveItems = [];
     for (let i = 0; i < listInner.children.length; i++) {
 
@@ -78,15 +70,21 @@ function saveMemo() {
         };
 
         saveItems.push(todoObj);
-
+        console.log(JSON.stringify(saveItems))
     }
+    localStorage.setItem('saved-items', JSON.stringify(saveItems));
+}
 
-    if (saveItems.length === 0) {
-        localStorage.removeItem('saved-items')
-    } else {
-        localStorage.setItem('saved-items', JSON.stringify(saveItems));
+const savedTodoList = JSON.parse(localStorage.getItem('saved-items'));
+console.log(savedTodoList);
+
+// //로컬에서 데이터 불러오기
+if (savedTodoList) { //로컬데이터가 존재하면 실행
+    for (let i = 0; i < savedTodoList.length; i++) {
+        plusContent(savedTodoList[i]);
     }
 }
+
 
 //엔터 눌렀을 때 리스트 추가
 function keyEnter() {
