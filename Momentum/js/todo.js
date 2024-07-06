5f415c318778f93af6352372e719bc3d44f4779c
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-07-06 16:06:10 +0900

    local 저장템 getItem하고 forEach 적용

--- Momentum/js/todo.js
+++ Momentum/js/todo.js
@@ -2,9 +2,11 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
 
-
-
+function saveToDos() {
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
 
 function deleteTodo(event) {
     const li = event.target.parentElement;
@@ -28,7 +30,25 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+// function sayHello(item) {
+//     console.log("this is the turn of", item);
+// }
+
+const savedToDos = localStorage.getItem("todos")
+// console.log(savedToDos); //string
+
+if (savedToDos !== null) {
+    const parsedTodos = JSON.parse(savedToDos);
+    // console.log(parsedTodos) //array
+    parsedTodos.forEach((item) => {
+        console.log("this is the turn of", item)
+    });//array의 각 item에 해당함수를 한번씩 실행
+}
+
