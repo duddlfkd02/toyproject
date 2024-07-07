a8819139ff65041d5ff3d4ef61fe4a1536615095
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-07-07 13:52:40 +0900

    todolist 삭제기능완료

--- Momentum/js/todo.js
+++ Momentum/js/todo.js
@@ -2,21 +2,27 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
 let toDos = [];
 
 function saveToDos() {
-    localStorage.setItem("todos", JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteTodo(event) {
     const li = event.target.parentElement;
     li.remove();
+    console.log(li)
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveToDos();
 }
 
 function paintToDo(newTodo) {
     const newLi = document.createElement("li");
+    newLi.id = newTodo.id;
     const newSpan = document.createElement("span");
-    newSpan.innerText = newTodo;
+    newSpan.innerText = newTodo.text;
     const newBtn = document.createElement("button");
     newBtn.innerText = "❎";
     newBtn.addEventListener("click", deleteTodo);
@@ -31,15 +37,19 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(), //각 li item을 구별하기 위해 작성
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
 
-const savedToDos = localStorage.getItem("todos")
+const savedToDos = localStorage.getItem(TODOS_KEY)
 
 
 if (savedToDos !== null) {
